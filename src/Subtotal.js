e743ea4c69bdbcd1a5643f6cff6da091c0e14abb
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-05-06 00:33:08 +0700

    part 3.5 more checkout pages

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import './Subtotal.css';
 import CurrencyFormat from 'react-currency-format'
+import { useStateValue } from './StateProvider';
+import { getBasketTotal } from './reducer';
 
 function Subtotal() {
+
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
         <div className="subtotal">
             <CurrencyFormat
                renderText = { (value)=> (
                 <>
                     <p>
-                        Subtotal (0 items);
-                        <strong> 0 </strong>
+                        Subtotal ( {basket?.length} items);
+                        <strong> {value} </strong>
                     </p>
                     <small className="subtotal__gift">
                         <input type="checkbox" />
@@ -19,7 +24,7 @@ function Subtotal() {
                 </>
                )}
             decimalScale={2}
-            value={0} // part of the homework
+            value={getBasketTotal(basket)} // part of the homework
             displayType={"text"}
             thousandSeparator={true}
             prefix={"$"}
--- src/reducer.js
+++ src/reducer.js
@@ -4,8 +4,11 @@ export const initialState = {
     basket: []
 };
 
+//Selector
+export const getBasketTotal = (basket) => 
+    basket?.reduce( (amount, item)=> item.price + amount, 0 );
+
 const reducer = (state, action) => {
-    console.log(action)
 
     switch (action.type) {
         case 'ADD_TO_BASKET':
@@ -14,6 +17,24 @@ const reducer = (state, action) => {
                 basket: [...state.basket, action.item],
             };
     
+        case "REMOVE_FROM_BASKET":
+
+            const index = state.basket.findIndex(
+                (basketItem)=> basketItem.id === action.id
+            )
+
+            let newBasket = [...state.basket];
+            if(index >= 0) {
+                newBasket.splice(index, 1);
+            }else {
+                
+            }
+
+            return {
+                ...state,
+                basket: newBasket
+            }
+
         default:
             return state;
     }
